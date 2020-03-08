3f9114cf8d92f7a0806e758a070a1927c498b925
StarBuckR <ridvantulemen@gmail.com>
2020-03-08 12:34:40 +0300

    add association between user and articles and debugger value

--- alpha-blog/db/schema.rb
+++ alpha-blog/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_08_084454) do
+ActiveRecord::Schema.define(version: 2020_03_08_091526) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
