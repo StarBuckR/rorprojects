f89ee035e25b8b3da7c40b78bfe894ca7cbb5e73
StarBuckR <ridvantulemen@gmail.com>
2020-03-08 13:28:06 +0300

    add secure password and attribute to users

--- alpha-blog/db/schema.rb
+++ alpha-blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_08_091526) do
+ActiveRecord::Schema.define(version: 2020_03_08_102352) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_03_08_091526) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
