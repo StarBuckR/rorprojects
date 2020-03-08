70cea1f7adc0c63e3368a69ebd99ec56982dba5f
StarBuckR <ridvantulemen@gmail.com>
2020-03-08 18:05:31 +0300

    Add users listing and update show article view

--- alpha-blog/app/controllers/users_controller.rb
+++ alpha-blog/app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+    
+    def index
+        @users = User.all
+    end
+
     def new
         @user = User.new
     end
@@ -29,7 +34,7 @@ class UsersController < ApplicationController
 
     def show
         @user = User.find(params[:id])
-        
+
     end
 
     private
--- alpha-blog/app/helpers/application_helper.rb
+++ alpha-blog/app/helpers/application_helper.rb
@@ -3,6 +3,6 @@ module ApplicationHelper
         gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
         size = options[:size]
         gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-        image_tag(gravatar_url, alt: user.username, class: "img-circle")
+        image_tag(gravatar_url, alt: user.username, class: "rounded-circle")
     end
 end
