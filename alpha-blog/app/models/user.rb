c00b7550e4a41c0c5f0882c2f7e872518b15eb7c
StarBuckR <ridvantulemen@gmail.com>
2020-03-12 18:18:07 +0300

    Add admin user functionality

--- alpha-blog/app/models/user.rb
+++ alpha-blog/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord 
-    has_many :articles
+    has_many :articles, dependent: :destroy # if the destroyed, delete all articles
     before_save { self.email = email.downcase }
     validates :username, presence: true,
                 uniqueness: { case_sensitive: false },
